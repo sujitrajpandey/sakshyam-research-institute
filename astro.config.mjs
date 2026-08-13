025094c6adae44ee3ecf1d37366e266a126c44d6
sujitrajpandey <sujit.raj.pandey@gmail.com>
2026-08-14 01:00:39 +0545

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,3 +0,0 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-export default defineConfig({ site: 'https://sakshyamresearch.org.np', integrations: [sitemap()] });
