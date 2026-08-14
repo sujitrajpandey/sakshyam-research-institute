c58233fe904b3e4a84a0bd8653a907bded9dfc64
sujitrajpandey <sujit.raj.pandey@gmail.com>
2026-08-14 09:39:42 +0545

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,16 +5,15 @@ export default {
     extend: {
       colors: {
         slate: {
-          900: '#344652', // Deep Slate from "Sr" and "SAKSHYAM"
+          900: '#344652', 
         },
         teal: {
-          600: '#459B9B', // Institute Teal from "RESEARCH INSTITUTE" banner
-          300: '#88C5C5', // Light Aqua from globe logo segments
+          600: '#459B9B', 
+          300: '#88C5C5', 
         },
         background: '#F8F9FA'
       },
       fontFamily: {
-        // You can link these to Google Fonts in your layout
         sans: ['Inter', 'sans-serif'], 
         heading: ['Merriweather', 'serif'], 
       }
