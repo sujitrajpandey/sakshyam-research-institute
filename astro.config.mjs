e77b4b8efb11c6f6a49e1dae9123549741545a8e
sujitrajpandey <sujit.raj.pandey@gmail.com>
2026-08-14 09:39:19 +0545

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,5 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://sakshyamresearch.org.np',
-  output: 'static',
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [tailwind()],
 });
